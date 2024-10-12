e670ee3bf96dd58934974a7a16302225247c0fa3
DH <danha3334@gmail.com>
2024-10-12 13:53:36 +0900

    add Meal_Manager.h

---
+++ Interactive_Intelligent_Kitchen_Help/Meal_Manager.h
@@ -0,0 +1,31 @@
+#ifndef MEAL_MANAGER_H
+#define MEAL_MANAGER_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class MealManager {
+public:
+    // Date 인스턴스의 벡터 (각 날짜에 대한 정보를 저장)
+    vector<Date*> dates;
+
+    // 생성자
+    MealManager();
+
+    // 달력 출력 및 각 작업을 수행할 수 있는 메인 메소드
+    void displayCalendar();
+
+    // 날짜 선택
+    void selectDate();
+
+    // 특정 기간의 모든 Meal 출력
+    void printMealsForDate();
+
+    // 특정 기간의 장보기 목록 출력
+    void printGrocery();
+};
+
+#endif
